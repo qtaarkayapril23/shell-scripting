fc22c31de6c875102f8f85b7f78a712cb2299c42
rajkumar <aarkay.gummadi@gmail.com>
2023-04-28 17:10:36 +0530

    Added the new changes in install jenkins file

--- 28april23/README.md
+++ 28april23/README.md
@@ -9,14 +9,22 @@ More on Scripting
 -----------------
 
 * According to last classes, we have to script to install jenkins
-```bash
-#!/bin/bash
-
+* [Refer Here](https://github.com/qtaarkayapril23/shell-scripting/blob/main/28april23/install-jenkins-java11.sh) for the script to install jenkins with java 11.
 ```
+sudo vi installjenkins.sh
+sudo sh installjenkins.sh
+```
+![Preview](Images/ss2.png)
+![Preview](Images/ss3.png)
 
-* Lets try to look at this script in a different way
+* Lets try to look at this script in a different way by drawing a flow chart
+![Preview](Images/ss1.png)
 
 * Lets try to draw a flow chart which will install jenkins on ubuntu and redhat systems
+![Preview](Images/ss4.png)
+
+* Below is the script to install jenkins on redhat as well as ubuntu systems
+* 
 
 ### Expression
 * An expression is combination of operands and operators which return some value
--- 28april23/install-jenkins.sh
+++ 28april23/install-jenkins.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 distribution=$(. /etc/os-release && echo $ID)
 echo $distribution
-# for ubuntu
-if [[ $distribuution == "ubuntu" ]]
-then 
-    apt update
-    apt install openjdk-11-jdk -y
+# for ubuntu 
+if  [[ $distribution == "ubuntu" ]]
+then
+    apt update 
+    apt install openjdk-17-jdk -y
     curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-      /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
     echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-      https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-      /etc/apt/sources.list.d/jenkins.list > /dev/null
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
     apt-get update
     apt-get install jenkins -y
-    java --version
-    jenkins --version
 elif [[ $distribution == "rhel" ]]
-then 
+then
     sudo yum install wget -y
     sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    ttps://pkg.jenkins.io/redhat-stable/jenkins.repo
+        https://pkg.jenkins.io/redhat-stable/jenkins.repo
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     sudo yum upgrade
     # Add required dependencies for the jenkins package
     sudo yum install java-11-openjdk
     sudo yum install jenkins
-    sudo systemctl daemon-reload  
+    sudo systemctl daemon-reload
 else
     echo "This script runs only on ubuntu and redhat"
-fi            
+fi
